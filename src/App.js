6bc44ff3b77cd6441c170c7dd760628e813b6fb2
Alain Perez Balart <alain.perez@doceleguas.com>
2021-03-26 23:18:26 -0400

    Added Portfolio section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/AboutMe";
+import Portfolio from "./components/Portfolio";
+
 function App() {
   return (
     <>
       <Navbar />
       <Header />
       <About />
+      <Portfolio />
     </>
   );
 }
---
+++ src/components/Portfolio.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import portfolio1 from "../portfolio_1.png";
+import portfolio2 from "../portfolio_2.png";
+import portfolio3 from "../portfolio_3.png";
+import portfolio4 from "../portfolio_4.png";
+const Portfolio = () => {
+    return (
+        <div className="portfolio-section">
+            <h1>Portafolio</h1>
+            <div className="item"><img className="portafolio-thumb" src={portfolio1} alt="portfolio"/></div>
+            <div className="item"><img className="portafolio-thumb" src={portfolio2} alt="portfolio"/></div>
+            <div className="item"><img className="portafolio-thumb" src={portfolio3} alt="portfolio"/></div>
+            <div className="item"><img className="portafolio-thumb" src={portfolio4} alt="portfolio"/></div>            
+        </div>
+    );
+}
+export default Portfolio
