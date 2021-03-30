8df43d230761a33eb3701c7dd6bcf8adb85464c4
Alain Perez Balart <alain.perez@doceleguas.com>
2021-03-30 17:48:43 -0400

    fixes type

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,7 +6,7 @@ import portfolio4 from "../portfolio_4.png";
 const Portfolio = () => {
     return (
         <div id="portfolio" className="portfolio-section">
-            <h1>Portafolio</h1>
+            <h1>Portfolio</h1>
             <div className="item"><img className="portafolio-thumb" src={portfolio1} alt="portfolio"/></div>
             <div className="item"><img className="portafolio-thumb" src={portfolio2} alt="portfolio"/></div>
             <div className="item"><img className="portafolio-thumb" src={portfolio3} alt="portfolio"/></div>
